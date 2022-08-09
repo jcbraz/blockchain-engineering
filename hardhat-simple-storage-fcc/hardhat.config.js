d52094b3991aa012b1e72af2873269b59efc19c1
José Braz <jose.braz@uphold.com>
2022-08-09 17:27:07 +0100

    Ongoing course

--- hardhat-simple-storage-fcc/hardhat.config.js
+++ hardhat-simple-storage-fcc/hardhat.config.js
@@ -2,12 +2,15 @@ require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 require("@nomiclabs/hardhat-etherscan");
 require("./tasks/block-number");
+require("hardhat-gas-reporter");
+require("solidity-coverage");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
-const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL;
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL || "https://eth-rinkeby";
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const COINMARKETCAP_KEY = process.env.COINMARKETCAP_KEY;
 
 
 // Every time we run a script, the hardhat network is deleted so we cannot interact with it afterwards.
@@ -32,6 +35,14 @@ module.exports = {
   solidity: "0.8.7",
   etherscan: {
     apiKey: ETHERSCAN_API_KEY
+  },
+  gasReporter: {
+    enabled: true,
+    outputFile: "gas-report.txt",
+    noColors: true,
+    currency: "USD",
+    coinmarketcap: COINMARKETCAP_KEY,
+    token: "OP" // deploy in a different network (Optimism for example)
   }
 };
 
---
+++ hardhat-simple-storage-fcc/test/test-deploy.js
@@ -0,0 +1,31 @@
+const { ethers } = require("hardhat")
+const { expect, assert } = require("chai")
+
+
+// describe("SimpleStorage", () => {})
+describe("SimpleStorage", function () {
+    let simpleStorageFactory, simpleStorage; // we declare the variables here so the variables can be used in the "it" scope
+    beforeEach(async function () {
+        simpleStorageFactory = await ethers.getContractFactory("SimpleStorage");
+        simpleStorage = await simpleStorageFactory.deploy();
+    }) // what to do before each one of "it"
+
+    it("Should start with a favorite number of 0", async function () {
+        const currentValue = await simpleStorage.retrieve();
+        const expectedValue = "0"
+        // We can use the keywords:
+            // assert
+            // expect
+        assert.equal(currentValue.toString(), expectedValue);
+        // expect(currentValue.toString().to.equal(expectedValue)); // same as assert
+    })
+
+    it("Should update when call store", async function () {
+        const expectedValue = "7";
+        const transactionResponse = await simpleStorage.store(expectedValue);
+        await transactionResponse.wait(1);
+
+        const currentValue = await simpleStorage.retrieve();
+        assert.equal(currentValue.toString(), expectedValue);
+    })
+}) // anonymous function syntax by convention
